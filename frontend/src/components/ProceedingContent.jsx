5496f3cb92eb9596392afc1a806fd291378d43f4
montserc <mrcatasus@gmail.com>
2021-09-14 19:07:50 +0200

    Muevo los datos de los proceedings al componente padre ya que los componentes hijos (lista y mapa) muestran los mismos datos

--- frontend/src/components/ProceedingContent.jsx
+++ frontend/src/components/ProceedingContent.jsx
@@ -1,20 +1,47 @@
-import React, { useState, createContext } from "react";
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { ScheduleContext } from "../pages/SchedulePage";
 import ProceedingList from "./ProceedingList";
 import ProceedingMap from "./ProceedingMap";
 import ProceedingSearch from "./ProceedingSearch";
+import * as api from "../api";
 import "./ProceedingContent.css";
 
 export const SearchContext = createContext();
 
 function ProceedingContent() {
+  const [proceedingData, setProceedingData] = useState(null);
   const [search, setSearch] = useState("");
   const [toggle, setToggle] = useState(true);
 
+  const { setProceedingIdSelected, visitAdded } = useContext(ScheduleContext);
+
+  const loadProceedingData = async () => {
+    try {
+      const proceedings = await api.getProceedings(
+        search !== "" ? `?${search}` : ""
+      );
+      if (proceedings) {
+        setProceedingData(proceedings);
+        setProceedingIdSelected(proceedings[0]._id);
+      }
+    } catch (err) {
+      console.log(err.toString());
+    }
+  };
+
+  useEffect(() => {
+    loadProceedingData();
+  }, [search, visitAdded]);
+
   let content;
-  if (toggle) {
-    content = <ProceedingList />;
+  if (proceedingData === null) {
+    content = <div>loading...</div>;
   } else {
-    content = <ProceedingMap />;
+    if (toggle) {
+      content = <ProceedingList proceedingData={proceedingData}/>;
+    } else {
+      content = <ProceedingMap proceedingData={proceedingData}/>;
+    }
   }
 
   return (
--- frontend/src/components/ProceedingList.jsx
+++ frontend/src/components/ProceedingList.jsx
@@ -1,33 +1,11 @@
-import React, { useEffect, useState, useContext } from "react";
-import { SearchContext } from "./ProceedingContent";
+import React, { useContext } from "react";
 import { ScheduleContext } from "../pages/SchedulePage";
 import { v4 as uuidv4 } from "uuid";
-import * as api from "../api";
 import "./ProceedingList.css";
 
-function ProceedingList() {
-  const [proceedingTable, setProceedingTable] = useState(null);
-  const { search } = useContext(SearchContext);
-  const { proceedingIdSelected, setProceedingIdSelected, visitAdded } =
+function ProceedingList({ proceedingData }) {
+  const { proceedingIdSelected, setProceedingIdSelected } =
     useContext(ScheduleContext);
-
-  const loadProceedingTable = async () => {
-    try {
-      const proceedings = await api.getProceedings(
-        search !== "" ? `?${search}` : ""
-      );
-      if (proceedings) {
-        setProceedingTable(proceedings);
-        setProceedingIdSelected(proceedings[0]._id);
-      }
-    } catch (err) {
-      console.log(err.toString());
-    }
-  };
-
-  useEffect(() => {
-    loadProceedingTable();
-  }, [search, visitAdded]);
 
   let table;
   let tableHeader;
@@ -41,51 +19,47 @@ function ProceedingList() {
     "CP",
     "Tipus de Procés",
     "Telèfons",
-  ];
-  if (proceedingTable === null) {
-    table = <div>loading...</div>;
-  } else {
-    table = (
-      <table className="table-list">
-        <thead>
-          <tr>
-            {tableHeader.map((header) => (
-              <th key={uuidv4()}>{header}</th>
-            ))}
-          </tr>
-        </thead>
-        <tbody>
-          {proceedingTable.map((proceeding) => {
-            requetDate = new Date(proceeding.request_date);
-            dd = requetDate.getDate();
-            dd = dd < 10 ? "0" + dd : dd;
-            mm = requetDate.getMonth() + 1;
-            mm = mm < 10 ? "0" + mm : mm;
-            return (
-              <tr key={proceeding._id}>
-                <td>
-                  <input
-                    type="radio"
-                    name="proceedings"
-                    value={proceeding._id}
-                    checked={proceeding._id === proceedingIdSelected}
-                    onChange={(e) => setProceedingIdSelected(e.target.value)}
-                  />
-                </td>
-                <td>{`${dd}/${mm}/${requetDate.getFullYear()}`}</td>
-                <td>{proceeding.proceeding_id}</td>
-                <td>{proceeding.name.first} {proceeding.name.last}</td>
-                <td>{proceeding.address.street}</td>
-                <td>{proceeding.address.postcode}</td>
-                <td>{proceeding.type}</td>
-                <td>{proceeding.phone_numbers.join(", ")}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  }
+  ];    
+  table = (
+    <table className="table-list">
+      <thead>
+        <tr>
+          {tableHeader.map((header) => (
+            <th key={uuidv4()}>{header}</th>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {proceedingData.map((proceeding) => {
+          requetDate = new Date(proceeding.request_date);
+          dd = requetDate.getDate();
+          dd = dd < 10 ? "0" + dd : dd;
+          mm = requetDate.getMonth() + 1;
+          mm = mm < 10 ? "0" + mm : mm;
+          return (
+            <tr key={proceeding._id}>
+              <td>
+                <input
+                  type="radio"
+                  name="proceedings"
+                  value={proceeding._id}
+                  checked={proceeding._id === proceedingIdSelected}
+                  onChange={(e) => setProceedingIdSelected(e.target.value)}
+                />
+              </td>
+              <td>{`${dd}/${mm}/${requetDate.getFullYear()}`}</td>
+              <td>{proceeding.proceeding_id}</td>
+              <td>{proceeding.name.first} {proceeding.name.last}</td>
+              <td>{proceeding.address.street}</td>
+              <td>{proceeding.address.postcode}</td>
+              <td>{proceeding.type}</td>
+              <td>{proceeding.phone_numbers.join(", ")}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 
   return (
     <div>
